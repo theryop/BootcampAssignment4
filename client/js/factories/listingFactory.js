c7257279de3e4b7e445527fed4b92f75f7df3852
theryop <theryop@outlook.com>
2018-09-25 21:07:35 -0400

    fixing factory ports

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,18 +1,18 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('http://localhost:'+process.env.PORT+'/api/listings');
+      return $http.get('http://localhost:8080/api/listings');
     },
 	
 	create: function(listing) {
-	  return $http.post('http://localhost:'+process.env.PORT+'/api/listings', listing);
+	  return $http.post('http://localhost:8080/api/listings', listing);
     }, 
 
     delete: function(id) {
 	   /**TODO
         return result of HTTP delete method
        */
-    return $http.delete('http://localhost:'+process.env.PORT+'/api/listings', id);
+    return $http.delete('http://localhost:8080/api/listings', id);
     }
   };
 
